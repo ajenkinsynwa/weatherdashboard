c7437f17bce7be61b399680e0c20c7b3d884a290
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-08-29 07:16:17 -0400

    here

---
+++ style.js
@@ -0,0 +1,59 @@
+function GetInfo() {
+    const newName = document.getElementById("cityInput");
+    const cityName = document.getElementById("cityName");
+
+    cityName.innerHTML = " "+ newName.value + " ";
+    
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=242b7b97630587c4368b58d6f02e6a80&units=imperial')
+
+.then(response => response.json())
+.then(data => {
+    
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min")
+        .innerHTML = "Min:" + Number(data.list[i].main.temp_min  )
+        .toFixed(1) + "°";
+     }
+        for(i = 0; i<5; i++){
+            document.getElementById("day" + (i + 1) + "Max")
+            .innerHTML = "Max: " + Number(data.list[i].main.temp_max )
+            .toFixed(2) + "°";
+    }
+
+       for(i = 0; i<5; i++){
+            document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+ 
+            data.list[i].weather[0].icon+".png";
+
+           
+            
+    }
+          console.log(data)
+
+})
+
+.catch(err => alert("Something Went Wrong"))
+}
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue = "Charlotte";
+    GetInfo();
+}
+
+var d =new Date();
+var weekday = ["Sunday","Monday","Tusday","Wednesday","Thursday","Friday","Saturday"];
+
+function CheckDay(day){
+    if(day +d.getDay() > 6){
+        return day +d.getDate()-7;
+
+    }
+
+    else{
+        return day +d.getDate();
+
+    }
+}
+
+for(i=0;i<5;i++){
+    document.getElementById("day" +(i+i)).innerHTML = weekday[Checkday(i)];
+}
