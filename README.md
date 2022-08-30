c9536a4f6b80525b442fce07137f551dd6a7d832
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-08-29 22:40:01 -0400

    readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# weatherdashboard
+# Weather Dashboard
 Challenge 6 Weather Dashboard 
+# Work Day Scheduler For Challenge 5 
+
+
+## Description 
+
+It is important to always check the weather to know what mother 
+nature has in store for the current day and next 5 days. It is also important 
+if you are going on a vacation to know the weather of your destination, that way
+you can prepare accordingly. This simple weather dashboard can 
+show you the 5 day forecast of your city of choice, max/min temeperature,
+humidity, and wind speed. Weather can change at any given moment but 
+using modern technoloy weather applications are very accurate. 
+
+## Visuls 
+
+![The Weather Dashboard Displays the 5 Day Forcast-Humidity-Wing-& Max/Min Temperature".](/weather.png)
+
+
+## Links to GitHub and Live Webpage 
+[Live Portfolio Webpage]()
+
+[Github Link](https://github.com/ajenkinsynwa/weatherdashboard)
--- style.js
+++ style.js
@@ -1,56 +1,55 @@
+// set a global variable 
+
+const searchedCities = [];
+
 function GetInfo() {
     const newName = document.getElementById("cityInput");
     const cityName = document.getElementById("cityName");
+    cityName.innerHTML = " " + newName.value + " ";
 
-    cityName.innerHTML = " "+ newName.value + " ";
+    //store newName in local storage
     
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=242b7b97630587c4368b58d6f02e6a80&units=imperial')
+    const searchedCity = newName.value;
+    searchedCities.push(searchedCity);
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
 
-.then(response => response.json())
-.then(data => {
-    
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Min")
-        .innerHTML = "Min:" + Number(data.list[i].main.temp_min  )
-        .toFixed(1) + "°";
-     }
-        for(i = 0; i<5; i++){
-            document.getElementById("day" + (i + 1) + "Max")
-            .innerHTML = "Max: " + Number(data.list[i].main.temp_max )
-            .toFixed(2) + "°";
-    }
 
-       for(i = 0; i<5; i++){
-            document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+ 
-            data.list[i].weather[0].icon+".png";
-
-           
-            
-    }
-          console.log(data)
-
-})
-
-.catch(err => alert("Something Went Wrong"))
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + newName.value + '&appid=242b7b97630587c4368b58d6f02e6a80&units=imperial')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            for (i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Min")
+                    .innerHTML = "Min:" + Number(data.list[i].main.temp_min)
+                        .toFixed(1) + "°";
+            }
+            for (i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Max")
+                    .innerHTML = "Max: " + Number(data.list[i].main.temp_max)
+                        .toFixed(2) + "°" + "<br>"+ "Humidity:" + data.list[i].main.humidity + "<br>"+ "Windspeed: " + data.list[i].wind.speed;
+            }
+            for (i = 0; i < 5; i++) {
+                document.getElementById("img" + (i + 1)).src = "http://openweathermap.org/img/wn/" +
+                    data.list[i].weather[0].icon + ".png";
+            }
+            console.log(data)
+        })
+        .catch(err => alert("Something Went Wrong"))
 }
 
-function DefaultScreen(){
+function DefaultScreen() {
     document.getElementById("cityInput").defaultValue = "Charlotte";
     GetInfo();
 }
 
-var d =new Date();
-var weekday = ["Sunday","Monday","Tusday","Wednesday","Thursday","Friday","Saturday"];
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tusday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-function CheckDay(day){
-    if(day +d.getDay() > 6){
-        return day +d.getDate()-7;
-
+function CheckDay(day) {
+    if (day + d.getDay() > 6) {
+        return day + d.getDate() - 7;
     }
-
-    else{
-        return day +d.getDate();
-
+    else {
+        return day + d.getDate();
     }
 }
-
